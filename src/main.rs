50aade11a5d6592773ffa16f697331d70607298a
Geert Fannes <geert.fannes@gmail.com>
2024-04-28 14:47:16 +0200

    Added basic directory walking

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 mod cli;
 
+use walkdir::WalkDir;
+
 fn main() {
     let args = cli::Args::parse();
     if args.do_log(1) {
         println!("{:?}", args);
+    }
+
+    for entry in WalkDir::new(args.root.as_str()) {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(entry) => {
+                if args.do_log(1) {
+                    println!("{}", entry.path().display())
+                }
+            }
+        }
     }
 }
