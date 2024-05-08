2d167cc831ba8eda00dd41b3408a09c019c9665d
Geert Fannes <geert.fannes@gmail.com>
2024-05-08 14:06:51 +0200

    Wip on FileInfo.add()

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,6 @@
 use crate::util::{Error, Result};
 use std::collections::HashMap;
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct FileInfo {
@@ -12,9 +13,20 @@ impl FileInfo {
             hash__info: HashMap::new(),
         }
     }
+    pub fn add(&mut self, dir: impl AsRef<Path>, rel: impl AsRef<Path>) -> Result<()>
+    {
+        let fp = dir.as_ref().join(rel);
+        let content = std::fs::read(&fp)?;
+        if let Some(prev) = self.hash__info.insert(fp.to_string_lossy().into_owned(), std::str::from_utf8(&content)?.to_owned()) {
+            fail!("File info already present for {}", fp.display());
+        }
+        Ok(())
+    }
 }
 
 #[test]
 fn test_new() {
     let mut fi = FileInfo::new();
+    fi.add(".", "Cargo.toml").unwrap();
+    println!("{:?}", fi);
 }
